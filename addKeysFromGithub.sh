fece56c8d9d95d862cd7eb0abf0065200e1e99bf
ponsfrilus <ponsfrilus@gmail.com>
2017-09-13 11:15:42 +0200

    Create dir if not exists

--- addKeysFromGithub.sh
+++ addKeysFromGithub.sh
@@ -2,14 +2,16 @@
 
 USAGE="--> Usage: sh $0 ghuser1 ghuser2 ghuser3 ..."
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
     echo ""
     echo ${USAGE}
     echo
     exit
 fi
 
-AUTHORIZED_KEYS=$HOME/.ssh/authorized_keys
+AUTHORIZED_KEYS_DIR=$HOME/.ssh
+mkdir -p $AUTHORIZED_KEYS_DIR
+AUTHORIZED_KEYS_PATH=$AUTHORIZED_KEYS_DIR/authorized_keys
 for USERNAME in "$@" ; do
-    wget https://github.com/$USERNAME.keys -O - >> $AUTHORIZED_KEYS
+    wget https://github.com/$USERNAME.keys -O - >> $AUTHORIZED_KEYS_PATH
 done
